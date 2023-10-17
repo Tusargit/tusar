bc2484fa0c164827b88f25ac6bacd59d4c5f7739
Tusar Mohanty <you@example.com>
2023-10-17 10:42:21 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Body from "./components/Body";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+
+
+function App() {
+  return (
+   <>
+   <Header/>
+   <Sidebar/>
+   <Body/>
+   </>
+  );
+}
+
+export default App;
